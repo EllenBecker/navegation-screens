17acf4469b87bf1cdabcebfd612d2312ea43d066
EllenBecker <91165522+EllenBecker@users.noreply.github.com>
2023-05-09 21:21:14 -0300

    Passing parameters by screens

--- app/src/main/java/com/example/navegationscreens/DetalheFragment.kt
+++ app/src/main/java/com/example/navegationscreens/DetalheFragment.kt
@@ -6,21 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private const val ARG_PARAM2 = "param2";
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetalheFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetalheFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var param1: String? = null;
+    private var param2: String? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,34 +27,31 @@ class DetalheFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var inflate = inflater.inflate(R.layout.fragment_principal,container, false);
+        val inflate = inflater.inflate(R.layout.fragment_detalhe,container, false);
 
-        var goToBackButton = inflate.findViewById(R.id.button) as Button;
+        val button = inflate.findViewById(R.id.button7) as Button;
 
-        goToBackButton.setOnClickListener{
+        val textView = inflate.findViewById(R.id.textView4) as TextView;
+        val textView2 = inflate.findViewById(R.id.textView5) as TextView;
+
+        textView.text = param1;
+        textView2.text = param2;
+
+        button.setOnClickListener{
             val fm = fragmentManager;
             fm!!.beginTransaction().replace(R.id.fragmentContainer,PrincipalFragment(),"PrincipalFragment")
-                .addToBackStack(null).commit()
+                .addToBackStack(null).commit();
         }
         return inflate;
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetalheFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             DetalheFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(ARG_PARAM1, param1);
+                    putString(ARG_PARAM2, param2);
                 }
             }
     }
--- app/src/main/java/com/example/navegationscreens/PrincipalFragment.kt
+++ app/src/main/java/com/example/navegationscreens/PrincipalFragment.kt
@@ -6,27 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PrincipalFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PrincipalFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
-    //private var param2: String? = null
+    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
-            //param2 = it.getString(ARG_PARAM2)
+            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -38,10 +32,13 @@ class PrincipalFragment : Fragment() {
 
         var goToButton = inflate.findViewById(R.id.button) as Button;
 
+        val texto = inflate.findViewById(R.id.editTextTextPersonName) as EditText;
+        val texto2 = inflate.findViewById(R.id.editTextTextPersonName2) as EditText;
 
         goToButton.setOnClickListener{
             val fm = fragmentManager;
-            fm!!.beginTransaction().replace(R.id.fragmentContainer,DetalheFragment(),"DetalheFragment")
+            fm!!.beginTransaction().replace(R.id.fragmentContainer,
+                DetalheFragment.newInstance(texto.text.toString(), texto2.text.toString()),"DetalheFragment")
                 .addToBackStack(null).commit()
         }
 
@@ -49,21 +46,11 @@ class PrincipalFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PrincipalFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(param1: String) =
             PrincipalFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(ARG_PARAM1, param1);
                 }
             }
     }
